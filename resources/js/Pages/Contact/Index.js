0366cddf686265c1113ef6b1bc1021bd43be4611
michel <ciardullomichel@gmail.com>
2022-03-15 16:59:19 +0100

    add contact

--- resources/js/Pages/Contact/Index.js
+++ resources/js/Pages/Contact/Index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Head } from '@inertiajs/inertia-react';
-import { Container } from 'react-bootstrap';
+import {Container, Form, Row, Col, Button, FloatingLabel} from 'react-bootstrap';
 
 import AppLayout from '@/Layouts/AppLayout';
 
@@ -12,8 +12,58 @@ export default function ContactIndex(props) {
 
             <Container className="mt-4">
 
-                <h1 className="mb-0">Contact</h1>
+                <h1 className="mb-0">Contactez-nous</h1>
                 <hr className="my-4"/>
+
+                <Form className="card card-body shadow">
+                    <Row className="mb-3 g-3">
+                        <Col>
+                            <FloatingLabel
+                                controlId="name"
+                                label="Nom"
+                            >
+                                <Form.Control type="text" placeholder="Nom" />
+                            </FloatingLabel>
+                        </Col>
+
+                        <Col>
+                            <FloatingLabel
+                                controlId="email"
+                                label="Adresse mail"
+                            >
+                                <Form.Control type="email" placeholder="Adresse mail" />
+                            </FloatingLabel>
+                        </Col>
+                    </Row>
+
+                    <FloatingLabel
+                        className="mb-3"
+                        controlId="subject"
+                        label="Sujet"
+                    >
+                        <Form.Control type="text" placeholder="Sujet" />
+                    </FloatingLabel>
+
+                    <FloatingLabel
+                        className="mb-3"
+                        controlId="message"
+                        label="Message"
+                    >
+                        <Form.Control
+                            type="text"
+                            placeholder="Message"
+                            as="textarea"
+                            style={{ height: '100px' }}
+                        />
+                    </FloatingLabel>
+
+                    <div>
+                        <Button variant="outline-primary" type="submit">
+                            Envoyer
+                        </Button>
+                    </div>
+
+                </Form>
 
             </Container>
         </AppLayout>
