c31eab15281146a5a514345a83322436485847f5
michel <ciardullomichel@gmail.com>
2022-03-20 21:44:52 +0100

    update pages esport

--- app/Http/Controllers/ESport/ESportController.php
+++ app/Http/Controllers/ESport/ESportController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\ESport;
 
 use App\Http\Controllers\Controller;
+use App\Models\Confrontation;
+use Illuminate\Support\Facades\Date;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -13,6 +15,14 @@ class ESportController extends Controller
      */
     public function index() : Response
     {
+        /*$currentDate = Date::now()->addDay(-2)->format('Y-m-d');
+
+        $confrontations = Confrontation::whereDate('date', '>=', $currentDate)
+            ->limit(10)
+            ->get();
+
+        dd($currentDate, $confrontations[0]->teams);*/
+
         return Inertia::render('ESport/Index');
     }
 }
--- resources/js/Pages/ESport/Index.js
+++ resources/js/Pages/ESport/Index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Head } from '@inertiajs/inertia-react';
-import { Container } from 'react-bootstrap';
+import {Badge, Container} from 'react-bootstrap';
 
 import AppLayout from '@/Layouts/AppLayout';
 
@@ -15,7 +15,44 @@ export default function ESportIndex(props) {
                 <h1 className="mb-0">ESports</h1>
                 <hr className="my-4"/>
 
+                <div className="card card-body nav-scroller">
+                    <div className="d-flex">
+                        <div className="position-relative">
+                            <Badge className="position-absolute top-0 end-0" variant="success">3</Badge>
+                            <svg width="100" height="100" fill="#42e0e4">
+                                <use xlinkHref="/img/sprite.svg#lol"/>
+                            </svg>
+                        </div>
+                        <div className="position-relative ms-3">
+                            <Badge className="position-absolute top-0 end-0" variant="success">3</Badge>
+                            <svg width="100" height="100" fill="#fe4a6d">
+                                <use xlinkHref="/img/sprite.svg#valorant"/>
+                            </svg>
+                        </div>
+                        <div className="position-relative ms-3">
+                            <Badge className="position-absolute top-0 end-0" variant="success">1</Badge>
+                            <svg width="100" height="100" fill="#a0b757">
+                                <use xlinkHref="/img/sprite.svg#csgo"/>
+                            </svg>
+                        </div>
+                        <div className="position-relative ms-3">
+                            <Badge className="position-absolute top-0 end-0" variant="success">1</Badge>
+                            <svg width="100" height="100" fill="#1aa3a1">
+                                <use xlinkHref="/img/sprite.svg#cd"/>
+                            </svg>
+                        </div>
+                    </div>
+                </div>
 
+                <div className="card mt-3">
+                    <header style={{height: '200px'}}>
+                        <img
+                            width="100%"
+                            height="100%"
+                            src="https://res.cloudinary.com/betengine/image/fetch/w_750,h_98,o_90,b_rgb:000000,c_fill,g_auto:face,f_webp/https://www.cloudbet.com/sports-data/entity-media/competitor/260198/image/2021-03-08-05-52-46"
+                            alt="csgo"/>
+                    </header>
+                </div>
             </Container>
         </AppLayout>
     );
