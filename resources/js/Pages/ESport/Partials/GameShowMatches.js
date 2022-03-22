495767ec2d8bf11c3828aaa20f9fe6a2505e0c0e
waypse <slavapankov20@gmail.com>
2022-03-22 21:41:23 +0100

    k

--- resources/js/Pages/ESport/Partials/GameShowMatches.js
+++ resources/js/Pages/ESport/Partials/GameShowMatches.js
@@ -16,22 +16,7 @@ function CardWrapper({ imgUrl, gameName }) {
         </div>
     )
 }
-function TabItem ({ eventKey, date, tourney, sprite, team1, team2, rating1, rating2 }) {
-    return (
-        <Accordion defaultActiveKey={["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]} alwaysOpen>
-            <TourneyCard
-                eventKey={eventKey}
-                date={date}
-                tourney={tourney}
-                sprite={sprite}
-                team1={team1}
-                team2={team2}
-                rating1={rating1}
-                rating2={rating2}
-            />
-        </Accordion>
-    )
-}
+
 function TourneyCard({ eventKey, date, tourney, sprite, team1, team2, rating1, rating2 }) {
     return (
         <Accordion.Item className="accordion-item-e-sport mb-3 shadow" eventKey={eventKey}>
@@ -71,9 +56,9 @@ function TourneyCard({ eventKey, date, tourney, sprite, team1, team2, rating1, r
                             <Button className="w-100" variant="dark" type="submit">{rating2}</Button>
                         </div>
                         <div className="d-flex flex-column justify-content-center align-items-center col-1">
-                            <div class="arrow">
-                                <div class="arrow-top"></div>
-                                <div class="arrow-bottom"></div>
+                            <div className="arrow">
+                                <div className="arrow-top"></div>
+                                <div className="arrow-bottom"></div>
                             </div>
                         </div>
                     </div>
@@ -82,6 +67,24 @@ function TourneyCard({ eventKey, date, tourney, sprite, team1, team2, rating1, r
         </Accordion.Item>
     )
 }
+
+function TabItem ({ eventKey, date, tourney, sprite, team1, team2, rating1, rating2 }) {
+    return (
+        <Accordion defaultActiveKey="0" alwaysOpen>
+            <TourneyCard
+                eventKey={eventKey}
+                date={date}
+                tourney={tourney}
+                sprite={sprite}
+                team1={team1}
+                team2={team2}
+                rating1={rating1}
+                rating2={rating2}
+            />
+        </Accordion>
+    )
+}
+
 export default function GameShowMatches(props) {
 
     return (
