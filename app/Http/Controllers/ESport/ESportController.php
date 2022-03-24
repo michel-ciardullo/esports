868781f9886dbb4e8301742ae123f43c745299e2
michel <ciardullomichel@gmail.com>
2022-03-24 13:04:41 +0100

    last update

--- app/Http/Controllers/ESport/ESportController.php
+++ app/Http/Controllers/ESport/ESportController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\ESport;
 
 use App\Http\Controllers\Controller;
-use App\Models\Confrontation;
-use App\Models\Game;
-use App\Models\Tournament;
+use App\Models\{
+    Confrontation,
+    Game,
+    Tournament
+};
 use Inertia\{
     Inertia,
     Response
@@ -13,16 +15,9 @@ use Inertia\{
 
 class ESportController extends Controller
 {
-    /**
-     * @return Response
-     */
-    public function index() : Response
+    private function postTreatment($games, $tournaments, $confrontations)
     {
-        $confrontations = Confrontation::with('teams', 'live')->where('date', '>=', date('Y-m-d'))->get();
-        $tournaments    = Tournament::whereIn('id', $confrontations->keys())->get();
-        $games          = Game::whereIn('id', $tournaments->keys())->get();
-
-        $today = $games->map(function ($game) use($tournaments, $confrontations)
+        return $games->map(function ($game) use($tournaments, $confrontations)
         {
             $tournaments = $tournaments->filter(fn ($tournament) => $tournament->game_id === $game->id)
                 ->map(function ($tournament) use($confrontations)
@@ -35,29 +30,52 @@ class ESportController extends Controller
 
             return array_merge($game->toArray(), compact('tournaments'));
         });
+    }
 
-        /*$today = [];
-        $confrontations->each(function ($confrontation, $game_id) use(&$today, $games, $tournaments)
+    /**
+     * @return Response
+     */
+    public function index() : Response
+    {
+        $confrontations = Confrontation::with('teams', 'live')/*->where('date', '>=', date('Y-m-d'))*/->get()->keyBy('id');
+        $tournaments    = Tournament::whereIn('id', $confrontations->keys())->get()->keyBy('id');
+        $games          = Game::whereIn('id', $tournaments->keys())->get()->keyBy('id');
+
+        $lives = [
+            'games'             => [],
+            'tournaments'       => [],
+            'confrontations'    => []
+        ];
+
+        $confrontations->each(function ($confrontation) use (&$lives, $tournaments)
         {
-            $tournament = $tournaments[$game_id];
-            $game       = $games[$game_id];
+            $tournament = $tournaments[$confrontation->tournament_id];
 
-            if (!array_key_exists($game_id, $today))
+            $push = function (&$arr, $tournament, $confrontation)
             {
-                $today[$game_id] = array_merge($game->toArray(), ['tournaments' => []]);
-            }
+                if (!array_search($tournament->game_id, $arr['games'], true))
+                {
+                    $arr['games'][] = $tournament->game_id;
+                }
 
-            if (!array_key_exists($tournament->id, $today[$game_id]['tournaments']))
+                if (!array_search($confrontation->tournament_id, $arr['tournaments'], true))
+                {
+                    $arr['tournaments'][] = $confrontation->tournament_id;
+                }
+
+                if (!array_search($confrontation->id, $arr['confrontations'], true))
+                {
+                    $arr['confrontations'][] = $confrontation->id;
+                }
+            };
+
+            if ($confrontation->status === 'live')
             {
-                $today[$game_id]['tournaments'][$tournament->id] = array_merge($tournament->toArray(), ['confrontations' => []]);
+                $push($lives, $tournament, $confrontation);
             }
-
-            $today[$game_id]['tournaments'][$tournament->id]['confrontations'][$confrontation->id] = $confrontation->toArray();
         });
 
-        dd($today);*/
-
-        return Inertia::render('Game/Show', compact('games', 'tournaments', 'confrontations', 'today'));
+        return Inertia::render('Game/Show', compact('confrontations', 'tournaments', 'games', 'lives'));
     }
 
     public function show(string $slug)
--- resources/js/Pages/Game/Show.js
+++ resources/js/Pages/Game/Show.js
@@ -4,8 +4,56 @@ import { Container } from 'react-bootstrap';
 
 import AppLayout from '@/Layouts/AppLayout';
 
-export default function GameShow({ auth }) {
+function ConfrontationList({ lives, confrontations })
+{
+    return lives['confrontations'].map(function (confrontation_id) {
+        return (
+            <span>
+                {confrontations[confrontation_id].status}
+            </span>
+        );
+    });
+}
 
+function TournamentList({ lives, tournaments, confrontations })
+{
+    return lives['tournaments'].map(function (tournament_id)
+    {
+        return (
+            <p>
+                {tournaments[tournament_id].name} :
+                <ConfrontationList
+                    lives={lives}
+                    confrontations={confrontations}
+                />
+            </p>
+        );
+    });
+}
+
+function LiveList({ lives, games, tournaments, confrontations })
+{
+    return lives['games'].map(function (game_id)
+    {
+        const tournaments_lives = lives['tournaments'].map(function (tournament_id)
+        {
+            return <TournamentList
+                lives={lives}
+                tournaments={tournaments}
+                confrontations={confrontations}
+            />;
+        });
+
+        return (
+            <div className="card mb-3">
+                <div className="card-header bg-primary">{games[game_id].name}</div>
+                <div className="card-body">{tournaments_lives}</div>
+            </div>
+        );
+    });
+}
+
+export default function GameShow({ auth, lives, games, tournaments, confrontations }) {
     return (
         <AppLayout auth={auth}>
             <Head title="Game show" />
@@ -14,6 +62,13 @@ export default function GameShow({ auth }) {
 
                 <h1 className="mb-0">Game show</h1>
                 <hr className="my-4"/>
+
+                <LiveList
+                    lives={lives}
+                    games={games}
+                    tournaments={tournaments}
+                    confrontations={confrontations}
+                />
 
             </Container>
         </AppLayout>
