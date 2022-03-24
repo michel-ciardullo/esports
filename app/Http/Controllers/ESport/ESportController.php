30df294eb7214fcead1b2a6456672441ccbdce74
michel <ciardullomichel@gmail.com>
2022-03-24 01:24:19 +0100

    update esports

--- app/Http/Controllers/ESport/ESportController.php
+++ app/Http/Controllers/ESport/ESportController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers\ESport;
 
 use App\Http\Controllers\Controller;
 use App\Models\Confrontation;
-use Carbon\Carbon;
+use App\Models\Game;
+use App\Models\Tournament;
 use Inertia\{
     Inertia,
     Response
@@ -17,25 +18,46 @@ class ESportController extends Controller
      */
     public function index() : Response
     {
-        $dateNow = Carbon::now();
+        $confrontations = Confrontation::with('teams', 'live')->where('date', '>=', date('Y-m-d'))->get();
+        $tournaments    = Tournament::whereIn('id', $confrontations->keys())->get();
+        $games          = Game::whereIn('id', $tournaments->keys())->get();
 
-        $data = Confrontation::with([
-                'tournament' => fn ($t) => $t->with('game'),
-                'teams',
-                'live',
-            ])
-            ->whereDate('confrontations.date', '>=', $dateNow)
-            ->whereTime('confrontations.time', '>=', $dateNow)
-            ->get();
-
-        $games = [];
-
-        foreach ($data as $value)
+        $today = $games->map(function ($game) use($tournaments, $confrontations)
         {
-            $game = $value->tournament->game;
-        }
+            $tournaments = $tournaments->filter(fn ($tournament) => $tournament->game_id === $game->id)
+                ->map(function ($tournament) use($confrontations)
+                {
+                    $confrontations = $confrontations->filter(fn ($confrontation) => $confrontation->tournament_id === $tournament->id)->all();
 
-        return Inertia::render('Game/Show', ['games' => $data]);
+                    return array_merge($tournament->toArray(), compact('confrontations'));
+                })
+                ->all();
+
+            return array_merge($game->toArray(), compact('tournaments'));
+        });
+
+        /*$today = [];
+        $confrontations->each(function ($confrontation, $game_id) use(&$today, $games, $tournaments)
+        {
+            $tournament = $tournaments[$game_id];
+            $game       = $games[$game_id];
+
+            if (!array_key_exists($game_id, $today))
+            {
+                $today[$game_id] = array_merge($game->toArray(), ['tournaments' => []]);
+            }
+
+            if (!array_key_exists($tournament->id, $today[$game_id]['tournaments']))
+            {
+                $today[$game_id]['tournaments'][$tournament->id] = array_merge($tournament->toArray(), ['confrontations' => []]);
+            }
+
+            $today[$game_id]['tournaments'][$tournament->id]['confrontations'][$confrontation->id] = $confrontation->toArray();
+        });
+
+        dd($today);*/
+
+        return Inertia::render('Game/Show', compact('games', 'tournaments', 'confrontations', 'today'));
     }
 
     public function show(string $slug)
